a0bcb704047a1bce9f3c6d15aafc9860228adbec
naker90 <AntonioJesusSG96@gmail.com>
2017-10-21 17:09:07 +0100

    refactor test - extract common variable

--- test/FizzBuzzShould.java
+++ test/FizzBuzzShould.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -5,16 +6,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FizzBuzzShould {
 
+    FizzBuzz fizzBuzz;
+
+    @Before
+    public void before(){
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void return_same_number_when_number_not_achieve_with_FizzBuzz_rules() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         MyNumber number = new MyNumber(1);
         assertThat(fizzBuzz.generate(number), is(number));
     }
 
     @Test
     public void return_Fizz_when_number_is_multiple_of_three() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         MyNumber number = new MyNumber(3);
         MyString string = new MyString("Fizz");
         assertThat(fizzBuzz.generate(number), is(string));
